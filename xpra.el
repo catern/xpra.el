0badddce8e0f6404875e42a882c7081251635ab4
Spencer Baugh <sbaugh@catern.com>
2025-02-22 09:31:52 -0500

    nginx too

--- xpra.el
+++ xpra.el
@@ -28,6 +28,12 @@ Set to 0 to disable.")
 (defvar xpra-buffer-name "*xpra server %s*"
   "Name for xpra buffers, with %s substituted for port number.")
 
+(defcustom xpra-nginx-exe "nginx"
+  "The nginx executable.")
+
+(defvar xpra-nginx-buffer-name "*xpra nginx*"
+  "Name for the xpra nginx buffer.")
+
 (defvar xpra-ssl-key nil
   "Filename of SSL key to use with xpra.
 
@@ -53,6 +59,52 @@ If nil, use self-signed certs.")
 		`((r0 . ,(random t)) (r1 . ,(random t)) (r2 . ,(random t)) (r3 . ,(random t))))
    t))
 
+(defun xpra--cd-to-dir ()
+  (let ((dir (locate-user-emacs-file "xpra")))
+    (make-directory dir t)
+    (set-file-modes dir #o700)
+    (setq default-directory dir)))
+
+(defun xpra--nginx-start ()
+  (interactive)
+  (with-current-buffer (get-buffer-create xpra-nginx-buffer-name)
+    (while-let ((proc (get-buffer-process (current-buffer))))
+      (kill-process proc)
+      (while (accept-process-output proc))
+      (sit-for 0.2)
+      (delete-process proc))
+    (comint-mode)
+    (erase-buffer)
+    (insert "xpra nginx server logs:\n")
+    ;; nginx must be in the xpra dir so that it can open the sockets.
+    (xpra--cd-to-dir)
+    (let ((keyname "key.pem")
+	  (certname "cert.pem"))
+      ;; These names are hardcoded in the nginx configuration.
+      (if (or xpra-ssl-key xpra-ssl-cert)
+	  (progn
+	    (make-symbolic-link xpra-ssl-key keyname t)
+	    (make-symbolic-link xpra-ssl-cert certname t))
+	(unless (and (file-exists-p keyname) (file-exists-p certname))
+	  (message "Generating self-signed certs.")
+	  (shell-command
+	   (concat
+	    "openssl req -x509 -newkey rsa:4096 -keyout " keyname " -out " certname " -sha256 -days 3650 -nodes -subj /C=US/CN=" fqdn))))
+      (cl-assert (file-exists-p keyname))
+      (cl-assert (file-exists-p certname)))
+    (make-symbolic-link "/home/sbaugh/src/xpra-html5/html5" "html" t)
+    (make-symbolic-link "/home/sbaugh/src/xpra.el/nginx.conf" "nginx.conf" t)
+    (make-process
+     :name "xpra-nginx"
+     :buffer (current-buffer)
+     :command
+     `(,xpra-nginx-exe
+       "-e" "/dev/stderr"
+       "-p" "."
+       "-c" "nginx.conf")
+     :connection-type 'pipe
+     :noquery t)))
+
 (defun xpra-start (&optional interactive)
   (interactive "p")
   (with-current-buffer (get-buffer-create (format xpra-buffer-name "new"))
@@ -61,10 +113,7 @@ If nil, use self-signed certs.")
     (comint-mode)
     (erase-buffer)
     (insert "xpra server logs:\n")
-    (let ((dir (locate-user-emacs-file "xpra")))
-      (make-directory dir t)
-      (set-file-modes dir #o700)
-      (setq default-directory dir))
+    (xpra--cd-to-dir)
     (let* ((fqdn (string-trim (shell-command-to-string "hostname --fqdn")))
 	   (sockname (format "emacs-x%s" (xpra--make-password)))
 	   (url (format "https://%s:10443/%s" fqdn sockname)))
